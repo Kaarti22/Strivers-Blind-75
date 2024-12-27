07269953f7007f7ef63714d4fa6ff0535b486380
Kaarti22 <kaartimondi@gmail.com>
2024-12-27 23:49:51 +0530

    sum of two integers

---
+++ sum_of_two_integers.cpp
@@ -0,0 +1,36 @@
+/*
+Link: https://leetcode.com/problems/sum-of-two-integers/description/
+
+Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+ 
+
+Example 1:
+
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+
+Input: a = 2, b = 3
+Output: 5
+ 
+
+Constraints:
+
+-1000 <= a, b <= 1000
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while(b != 0){
+            int temp = a;
+            a = a ^ b;
+            b = (temp & b) << 1;
+        }
+        return a;
+    }
+};
